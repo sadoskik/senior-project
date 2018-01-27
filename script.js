8e2a5941ced8467637b763aeaed4691044d0a5cc
meganrich <33968732+meganrich@users.noreply.github.com>
2018-01-27 15:19:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var facts = [
+  {year:1431, event:"Joan of Arc is executed by burning."},
+  {year:1487, event:"The House of Lancaster defeats the House of York in the War of the Roses."}
+]
+
+var test = 42;
