d0b305482b0ecc2cb3e1b52cbfdd5168eee9a002
sadoskik <sadoskik@gmail.com>
2018-03-05 22:20:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ var facts = [
   {year:"Prohibition", event:"What era does this book take place in?"},
   {year:"Massachusetts", event:"What state does this book take place in?"},
   {year:"Devil's Reef", event:"What's the name of the ocean-based mass that causes the strife?"},
+  {year:"Antiquarian", event:"Lovecraft felt he was this and many of his characters held this hobby"},
+  {year:"Cosmocism", event:"Lovecraft started this movement"},
+  {year:"Xenophobia", event:"This theme is shown by the main character's innate hatred of the townspeople's characteristics"}
   
   
 ];
