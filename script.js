aaa1e26a67542b0804e2fb675a19dfaf81f0c286
sadoskik <sadoskik@gmail.com>
2018-03-05 21:19:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var facts = [
   {year:"Prohibition", event:"What era does this book take place in?"},
   {year:"Massachusetts", event:"What state does this book take place in?"},
   {year:"Devil's Reef", event:"What's the name of the ocean-based mass that causes the strife?"},
-  {year:1440, event:"Johannes Gutenberg invents the printing press."},
-  {year:1487, event:"The House of Lancaster defeats the House of York in the War of the Roses."},
+  
   
 ];
