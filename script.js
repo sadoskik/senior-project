66c32bbec471df4df9cf951ed7158cb2dcaebe13
sadoskik <sadoskik@gmail.com>
2018-03-05 20:48:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var facts = [
   {year:"H.P. Lovecraft" , event:"Who is the author of our book?"},
   {year:"Arkham", event:"To what city is the speaker ultimately trying to travel?"},
-  {year:"Prohibition, event:"What era does this book take place in?"},
+  {year:"Prohibition", event:"What era does this book take place in?"},
   {year:"Massachusetts", event:"What state does this book take place in?"},
   {year:"Devil's Reef", event:"What's the name of the ocean-based mass that causes the strife?"},
   {year:1440, event:"Johannes Gutenberg invents the printing press."},
